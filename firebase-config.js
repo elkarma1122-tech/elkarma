c728a8b5dfad1fb1919b83d4430e5b5c7292de25
elkarma1122-tech <elkarma1122@gmail.com>
2026-08-26 13:09:05 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCMCQE7Lc5vD2dFGAbayqhfc_kWSyHa5NU",
   authDomain: "elkarma-47cfd.firebaseapp.com",
+  databaseURL: "https://elkarma-47cfd-default-rtdb.firebaseio.com",
   projectId: "elkarma-47cfd",
   storageBucket: "elkarma-47cfd.firebasestorage.app",
   messagingSenderId: "398752188707",
   appId: "1:398752188707:web:41fb3e46a026a24eb4a496"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { app, auth, db };
